cdc6f1195362bf45746d3868949e840b66f11023
chana2000 <christine.nausch@kabelmail.de>
2022-08-28 19:53:38 +0200

    envChristine angelegt

---
+++ pjshyperbot/envChristine/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\HP\Source\Repos\JMU-Information-Systems\pjs-hyperbot-main\pjshyperbot\envChristine\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
