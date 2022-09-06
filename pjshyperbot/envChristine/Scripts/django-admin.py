134e969ac44ca1c34daab563c2869206b858773a
chana2000 <christine.nausch@kabelmail.de>
2022-09-06 15:03:56 +0200

    envChristine removed from repository

--- pjshyperbot/envChristine/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\HP\Source\Repos\JMU-Information-Systems\pjs-hyperbot-main\pjshyperbot\envChristine\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
