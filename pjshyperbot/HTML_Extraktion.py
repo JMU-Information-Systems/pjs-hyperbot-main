40bc430c650da65a0f9aa20810b1ed8b13ed7485
chana2000 <christine.nausch@kabelmail.de>
2022-09-10 16:02:22 +0200

    HTML Extraktion: 2-dimensionaler Array mit Variablenname und Wert

--- pjshyperbot/HTML_Extraktion.py
+++ pjshyperbot/HTML_Extraktion.py
@@ -8,58 +8,67 @@ import urllib.request
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.request import urlopen
+import numpy as np
 
-chrome_options.add_argument("--headless")
-PATH="C:\Program Files (x86)\chromedriver.exe"
-driver=webdriver.Chrome(PATH)
+class HTMLExtraction():
 
-driver.get("http://132.187.226.138:8080/webapp/view/common/login.page")
-loginurl="http://132.187.226.138:8080/webapp/view/groupware/task/taskDetail.page?entityId=9407"
+    def getVariables(self):
+        chrome_options.add_argument("--headless")
+        PATH="C:\Program Files (x86)\chromedriver.exe"
+        driver=webdriver.Chrome(PATH)
 
-username="rpa.team22@gmail.com"
-password="RPA2022#"
+        driver.get("http://132.187.226.138:8080/webapp/view/common/login.page")
+        loginurl="http://132.187.226.138:8080/webapp/view/groupware/task/taskDetail.page?entityId=9407"
 
-driver.find_element("name", "loginForm:username").send_keys(username)
-driver.find_element("name", "loginForm:password").send_keys(password)
-driver.find_element("name", "loginForm:remember").click()
-driver.find_element("id", "loginForm:loginButton").click()
-time.sleep(2)
-driver.get(loginurl)
-time.sleep(4)
-soup = BeautifulSoup(driver.page_source, features="html.parser")
+        username="rpa.team22@gmail.com"
+        password="RPA2022#"
 
-#hh=soup.find_all("div", {"id": "pageForm:taskDescriptionXRow"})
-hh=soup.find("textarea", {"id": "pageForm:taskDescriptionX_TA"})
-#for e in hh.findAll('br'):
-    #e.extract()
-hh=hh.text
-hh=hh.replace('<br>','\n')
-hh=hh.replace('<p>','\n')
+        driver.find_element("name", "loginForm:username").send_keys(username)
+        driver.find_element("name", "loginForm:password").send_keys(password)
+        driver.find_element("name", "loginForm:remember").click()
+        driver.find_element("id", "loginForm:loginButton").click()
+        time.sleep(2)
+        driver.get(loginurl)
+        time.sleep(4)
+        soup = BeautifulSoup(driver.page_source, features="html.parser")
 
-#print(hh)
+        #hh=soup.find_all("div", {"id": "pageForm:taskDescriptionXRow"})
+        hh=soup.find("textarea", {"id": "pageForm:taskDescriptionX_TA"})
+        #for e in hh.findAll('br'):
+            #e.extract()
 
-import re
-txt = hh
-x = re.findall("\:(.*)",txt)
-y = re.findall("(.*):",txt)
+        
+        hh=hh.text
 
-#Ersteller=(x[0])
-#Ankunftszeit=(x[1])
-#Zielort=(x[2])
-#Adresse des Hotels=(x[3])
-#Ausland=(x[4])
-#Enddatum=(x[5])
-#Uhrzeit=(x[6])
+        #print("hh" + str(hh))
+        hh=hh.replace('<br>','\n')
+        hh=hh.replace('<p>','\n')
 
+        #print(hh)
 
-#print(x)
-print(x[0])
-print(x[1])
-print(x[2])
-print(x[3])
-print(x[4])
-print(x[5])
-print(x[6])
+        import re
+        txt = hh
+
+        #print("Txt" + txt)
+
+        value = re.findall("\:(.*)",txt)
+        variableName = re.findall("(.*):",txt)
+        
+
+        combined = np.column_stack((variableName, value))
+
+        print(combined[0][0])
+
+        #Ersteller=(x[0])
+        #Ankunftszeit=(x[1])
+        #Zielort=(x[2])
+        #Adresse des Hotels=(x[3])
+        #Ausland=(x[4])
+        #Enddatum=(x[5])
+        #Uhrzeit=(x[6])
+        
+
+        return (combined)
 
 
 
