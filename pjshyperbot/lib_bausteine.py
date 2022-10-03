696151ae91440108e09c6769e373d91ffd6d1439
lgerkens <leonie.gerkens@stud-mail.uni-wuerzburg.de>
2022-10-03 16:24:53 +0200

    Create Variable Sequence

--- pjshyperbot/lib_bausteine.py
+++ pjshyperbot/lib_bausteine.py
@@ -38,6 +38,15 @@ def sequence(xaml): #write sequence header of UiPath XAML file
     xaml.write("      <scg:Dictionary x:TypeArguments=\"x:String, x:Object\">\n        <x:Boolean x:Key=\"IsExpanded\">True</x:Boolean>\n      </scg:Dictionary>\n    </sap:WorkflowViewStateService.ViewState>\n")
     return("  </Sequence>\n")
 
+def s_varaibles(xaml):
+    xaml.write("	  <Sequence.Variables>\n")
+    return("	  </Sequence.Variables>\n")    
+
+def variable(xaml, vname, vtype, vinit):
+    xaml.write("		<Variable x:TypeArguments=\"x:" + vtype + "\" Name=\"" + vname + "\">\n")
+    xaml.write("		  <Variable.Default>\n			<Literal x:TypeArguments=\"x:" + vtype + "\">" + vinit + "</Literal>\n		  </Variable.Default>\n")
+    xaml.write("		</Variable>")
+
 def a_comment(xaml,id,txt): #kommentar
     xaml.write("    <ui:Comment sap2010:WorkflowViewState.IdRef=\"Comment_" + id + "\" Text=\"" + txt + "\" />\n")
 
@@ -49,18 +58,18 @@ def a_comment_calendar_picker (xaml):
 
 #Open Browser Aktivitäten
 
-def a_attach_browser(xaml, url): #auf bereits geöffnetes Fenster zurückspringen, wenn User bspw. von Browser zu Excel und dann wieder zum browser wechselt
-    xaml.write("    <ui:BrowserScope BrowserType=\"Edge\" ContinueOnError=\"True\" DisplayName=\"Attach Browser\" sap:VirtualizedContainerService.HintSize=\"434,230.666666666667\" sap2010:WorkflowViewState.IdRef=\"BrowserScope_1\"  Selector=\"&lt;html app=\'msedge.exe\' url=\'"+url+"\'/&gt;\" TimeoutMS=\"1000\">\n")
-    xaml.write("      <ui:BrowserScope.Body>\n")
-    xaml.write("        <ActivityAction x:TypeArguments=\"x:Object\">\n")
-    xaml.write("          <ActivityAction.Argument>\n")
-    xaml.write("            <DelegateInArgument x:TypeArguments=\"x:Object\" Name=\"ContextTarget\" />\n")
-    xaml.write("          </ActivityAction.Argument>\n")
-    xaml.write("          <Sequence DisplayName=\"Do\">\n")
-    xaml.write("          </Sequence>\n")
-    xaml.write("        </ActivityAction>\n")
-    xaml.write("      </ui:BrowserScope.Body>\n")
-    xaml.write("    </ui:BrowserScope>\n")
+#def a_attach_browser(xaml, url): #auf bereits geöffnetes Fenster zurückspringen, wenn User bspw. von Browser zu Excel und dann wieder zum browser wechselt
+    #xaml.write("    <ui:BrowserScope BrowserType=\"Edge\" ContinueOnError=\"True\" DisplayName=\"Attach Browser\" sap:VirtualizedContainerService.HintSize=\"434,230.666666666667\" sap2010:WorkflowViewState.IdRef=\"BrowserScope_1\"  Selector=\"&lt;html app=\'msedge.exe\' url=\'"+url+"\'/&gt;\" TimeoutMS=\"1000\">\n")
+    #xaml.write("      <ui:BrowserScope.Body>\n")
+    #xaml.write("        <ActivityAction x:TypeArguments=\"x:Object\">\n")
+    #xaml.write("          <ActivityAction.Argument>\n")
+    #xaml.write("            <DelegateInArgument x:TypeArguments=\"x:Object\" Name=\"ContextTarget\" />\n")
+    #xaml.write("          </ActivityAction.Argument>\n")
+    #xaml.write("          <Sequence DisplayName=\"Do\">\n")
+    #xaml.write("          </Sequence>\n")
+    #xaml.write("        </ActivityAction>\n")
+    #xaml.write("      </ui:BrowserScope.Body>\n")
+    #xaml.write("    </ui:BrowserScope>\n")
 
 def a_navigate_to (xaml, url): #muss im selben browser sein
     xaml.write("            <ui:NavigateTo Browser=\"{x:Null}\" DisplayName=\"Navigate To\" Url=\""+url+"\" />\n")
