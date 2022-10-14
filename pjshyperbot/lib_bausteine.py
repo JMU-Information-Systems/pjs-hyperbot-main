1f218a84eacda7d601662c9bd02fcaa6e5010bbc
lgerkens <leonie.gerkens@stud-mail.uni-wuerzburg.de>
2022-10-14 16:23:11 +0200

    Update Bausteine

--- pjshyperbot/lib_bausteine.py
+++ pjshyperbot/lib_bausteine.py
@@ -53,23 +53,6 @@ def a_comment(xaml,id,txt): #kommentar
 def a_comment_calendar_picker (xaml):
     xaml.write("            <ui:Comment Text=\"Kalender Picker: Es wurde im Prozessablauf ein Datepicker verwendet. Bitte pruefen, ob Datum hier manuell eingegeben werden kann. Wenn ja, dann ganz normal mit Type Into. Ansonsten bitte Klickaktivität manuell einbauen mit dynamischem Selektor, der entsprechend das gewünschte Datum auswählen kann. &#xD;&#xA;&#xD;&#xA;Wichtig: Es kann sein, dass der Monat mit dem gewünschten Datum nicht direkt angezeigt wird, wenn der Kalender geöffnet ist. &#xD;&#xA;&#xD;&#xA;Dann so vorgehen:&#xD;&#xA;&#xD;&#xA;Try Catch Aktivität einbauen, innerhalb einer while Schleife:&#xD;&#xA;&#xD;&#xA;while Schleife: Bedingung&#xD;&#xA;&#xD;&#xA;im Try: &#xD;&#xA;Klickaktivit?t mit gewünschtem Datum, versucht Feld zu finden&#xD;&#xA;dann: while schleifen Bedingung auf True setzen&#xD;&#xA;&#xD;&#xA;im except: Klickaktivität auf &quot;Weiter&quot;, bis gewünschter Monat angezeigt wird&#xD;&#xA;&#xD;&#xA;While Schleife: Läuft solange bis Try Aktivität erfolgreich durchgeführt wurde\" />")
 
-#def a_comment_optionsfeld (xaml):
-    #xaml.write("            <ui:Comment Text=\"Optionsfeld: Hier k?nnte evtl. auch eine Select Item Aktivit?t sinnvoll sein\" />")
-
-#Open Browser Aktivit?ten
-
-#def a_attach_browser(xaml, url): #auf bereits ge?ffnetes Fenster zur?ckspringen, wenn User bspw. von Browser zu Excel und dann wieder zum browser wechselt
-    #xaml.write("    <ui:BrowserScope BrowserType=\"Edge\" ContinueOnError=\"True\" DisplayName=\"Attach Browser\" sap:VirtualizedContainerService.HintSize=\"434,230.666666666667\" sap2010:WorkflowViewState.IdRef=\"BrowserScope_1\"  Selector=\"&lt;html app=\'msedge.exe\' url=\'"+url+"\'/&gt;\" TimeoutMS=\"1000\">\n")
-    #xaml.write("      <ui:BrowserScope.Body>\n")
-    #xaml.write("        <ActivityAction x:TypeArguments=\"x:Object\">\n")
-    #xaml.write("          <ActivityAction.Argument>\n")
-    #xaml.write("            <DelegateInArgument x:TypeArguments=\"x:Object\" Name=\"ContextTarget\" />\n")
-    #xaml.write("          </ActivityAction.Argument>\n")
-    #xaml.write("          <Sequence DisplayName=\"Do\">\n")
-    #xaml.write("          </Sequence>\n")
-    #xaml.write("        </ActivityAction>\n")
-    #xaml.write("      </ui:BrowserScope.Body>\n")
-    #xaml.write("    </ui:BrowserScope>\n")
 
 def a_navigate_to (xaml, url): #muss im selben browser sein
     xaml.write("            <ui:NavigateTo Browser=\"{x:Null}\" DisplayName=\"Navigate To\" Url=\""+url+"\" />\n")
